a54dcd0f26c43073ab7c552058976461ac30c2d9
Dustin Newbold <Dustin.Newbold@CookMedical.com>
2015-10-19 11:24:38 -0400

    Fixed bug where find wouldnt work properly if directories had spaces in them

--- suspf.sh
+++ suspf.sh
@@ -36,25 +36,25 @@ echo
 
 echo "Recently edited files"
 echo "---------------------"
-find $directory -type f -iname '*.php*' -user $apacheUser -mtime -7
+find "$directory" -type f -iname '*.php*' -user $apacheUser -mtime -7
 echo
 echo
 
 echo "Apache files with eval()"
 echo "------------------------"
-find $directory -type f -iname '*.php*' -user $apacheUser | xargs grep -l "eval *(" --color
+find "$directory" -type f -iname '*.php*' -user $apacheUser -print0 | xargs -0 grep -l "eval *(" --color
 echo
 echo
 
 echo "Apache files with base64_decode"
 echo "------------------------"
-find $directory -type f -iname '*.php*' -user $apacheUser | xargs grep -l "base64_decode *(" --color
+find "$directory" -type f -iname '*.php*' -user $apacheUser -print0 | xargs -0 grep -l "base64_decode *(" --color
 echo
 echo
 
 echo "Apache files with gzinflate()"
 echo "------------------------"
-find $directory -type f -iname '*.php*' -user $apacheUser | xargs grep -l "gzinflate *(" --color
+find "$directory" -type f -iname '*.php*' -user $apacheUser -print0 | xargs -0 grep -l "gzinflate *(" --color
 echo
 echo
 
@@ -63,10 +63,10 @@ echo "All PHP files in a writable directory"
 echo "------------------------"
 writable_dirs=$(find $directory -type d \( -perm -o+w -or \( -perm -g+w -group $apacheGroup \) -or \( -perm -g+w -group $apacheGroup \) -or -user $apacheUser \))
  
-for dir in $writable_dirs
+for dir in "$writable_dirs"
 do
-	#echo $dir
-	find $dir -type f -name '*.php'
+	# echo "Checking directory: $dir"
+	find "$dir" -type f -iname '*.php*'
 done
 echo
 echo
