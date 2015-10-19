d2b2547997d06c62956ea55d2df444db374197c3
Dustin Newbold <Dustin.Newbold@CookMedical.com>
2015-10-19 11:12:38 -0400

    Added ability to change variables via CLI

--- suspf.sh
+++ suspf.sh
@@ -3,10 +3,33 @@ apacheUser="apache"
 apacheGroup="apache"
 directory="."
 
+##### Set defaults #####
+if [[ $# -gt 0 ]]; then
+	# Directory
+	if [[ ! -z "$1" ]]; then
+		directory="$1"
+	fi
+
+	# Apache User
+	if [[ ! -z "$2" ]]; then
+		apacheUser="$2"
+	fi
+
+	# Apache Group
+	if [[ ! -z "$3" ]]; then
+		apacheGroup="$3"
+	fi
+fi
+
 echo "============================"
 echo "|  Suspicious File Finder  |"
 echo "|              by Dustin   |"
 echo "============================"
+echo
+echo "Using the following variables:"
+echo "Directory: $directory"
+echo "Apache User: $apacheUser"
+echo "Apache Group: $apacheGroup"
 echo
 echo
 
@@ -42,8 +65,8 @@ writable_dirs=$(find . -type d \( -perm -o+w -or \( -perm -g+w -group $apacheGro
  
 for dir in $writable_dirs
 do
-    #echo $dir
-    find $dir -type f -name '*.php'
+	#echo $dir
+	find $dir -type f -name '*.php'
 done
 echo
 echo
